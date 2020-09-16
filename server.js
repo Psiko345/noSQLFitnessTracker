bf7c00970b0c36a85a1851487734752e4e516ddf
Joshua Piper <joshua.g.piper@gmail.com>
2020-09-16 13:23:31 +1000

    restructured server file to work properly.

--- server.js
+++ server.js
@@ -3,22 +3,25 @@ const express = require("express");
 const compression = require("compression");
 const logger = require("morgan");
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-});
-
 const app = express();
+const PORT = process.env.PORT || 8080;
+
 app.use(logger("dev"))
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
 app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
+
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
 
-const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}!`);
 });
